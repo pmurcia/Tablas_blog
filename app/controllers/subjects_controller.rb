0af27c436299c35452105b35597ea55ba5d7cf1b
pmurcia <p.murcia.morilla@gmail.com>
2014-09-05 20:24:29 +0000

    Add show action for subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,9 @@
 class SubjectsController < ApplicationController
   def index
-  	@subjects = Subject.all
+  	@subjects = Subject.all.order(created_at: :asc)
+  end
+
+  def show
+  	@subject = Subject.find params[:id]
   end
 end
