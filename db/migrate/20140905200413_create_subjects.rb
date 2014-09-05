4da46a5abdefd77b562d79cdbc0cca1680369ad1
pmurcia <p.murcia.morilla@gmail.com>
2014-09-05 20:15:11 +0000

    Add maths seed

--- db/migrate/20140905200413_create_subjects.rb
+++ db/migrate/20140905200413_create_subjects.rb
@@ -1,8 +1,8 @@
 class CreateSubjects < ActiveRecord::Migration
   def change
     create_table :subjects do |t|
-
-      t.timestamps
+      t.string :name
+      # t.timestamps
     end
   end
 end
