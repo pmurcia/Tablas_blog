bc573ab147ba3c6b9582d88f700bddd67a589c51
pmurcia <p.murcia.morilla@gmail.com>
2014-09-11 19:41:38 +0000

    Add to every model friendly:id slug

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course < ActiveRecord::Base
   resourcify
   belongs_to :subject
   has_many :posts
+  
+  friendly_id :name, use: :slugged
 
   validates_uniqueness_of :name, scope: :subject_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :course
+
+  friendly_id :title, use: :slugged
 end
