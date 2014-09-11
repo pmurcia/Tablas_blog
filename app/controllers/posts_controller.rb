8f6645ab9d52ad8f6b3dab064cde05108dad63bd
pmurcia <p.murcia.morilla@gmail.com>
2014-09-11 15:29:33 +0000

    Add show view of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,17 @@ class PostsController < ApplicationController
   before_action :set_course!
 
   def index
-  	@posts = Post.all
+  	@posts = @course.posts.all
+  end
+
+  def show
+  	@post = @course.posts.find params[:id]
   end
 
   private
 
   def set_course!
-
+  	@subject = Subject.find params[:subject_id]
+  	@course = @subject.courses.find params[:course_id]
   end
 end
