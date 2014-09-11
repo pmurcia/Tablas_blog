49323dbb12db8422ad5dbec388f8cc064ccf4062
pmurcia <p.murcia.morilla@gmail.com>
2014-09-11 18:05:09 +0000

    Can create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,22 @@ class PostsController < ApplicationController
   	@post = @course.posts.find params[:id]
   end
 
+  def new
+    @post = @course.posts.new
+  end
+
+  def create
+    @post = @course.posts.create title: params[:post][:title], content: params[:post][:content]
+
+    if @post.save
+      flash[:notice] = 'Creada satisfactoriamente'
+      redirect_to action: 'show', controller: 'courses', subject_id: @subject, id: @course
+    else
+      flash[:alert] = 'No se ha podido crear'
+      render 'new'
+    end
+  end
+
   private
 
   def set_course!
